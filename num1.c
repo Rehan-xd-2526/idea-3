c1a1196d7925efa8aecbd3fc61dd56eae8c8118a
Rehan-xd-2526 <rehannaushad786@gmail.com>
2025-10-09 10:27:53 +0530

    Add files via upload

---
+++ num1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+ int main(){
+    int num;
+    printf("enter number: ");
+    scanf("%d",&num);
+
+    if(num>0){
+        printf("%d is positive",num);
+    }else if(num<0)
+    {
+        printf("%d is negative",num); 
+    }else
+   {
+    printf("%d is 0",num);
+}
+   return 0;
+
+ }
